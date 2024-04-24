bdf66c34b81d61e229241552ab4b80fef99767bc
Tushar Tari <tushar.tari.external@telefonica.com>
2024-04-24 20:49:17 +0200

    First Commit

---
+++ src/main/java/com/telefonica/prepaid/restaurantlisting/mapper/RestaurantMapper.java
@@ -0,0 +1,17 @@
+package com.telefonica.prepaid.restaurantlisting.mapper;
+
+import com.telefonica.prepaid.restaurantlisting.dto.RestaurantDTO;
+import com.telefonica.prepaid.restaurantlisting.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
